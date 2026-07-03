397a065b264c4a47ba1f8f5901b1a37e4d4c396d
Em Karisch <emii.khaos@gmail.com>
2026-07-04 00:15:03 +0200

    feat: add burner biochamber

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,3 +1,4 @@
+require("prototypes.content.burner-biochamber")
 require("prototypes.content.carbonut-agriculture")
 require("prototypes.content.wood-logistics.compat")
 require("prototypes.planets.lignumis.compat")
---
+++ prototypes/content/burner-biochamber.lua
@@ -0,0 +1,32 @@
+khaoslib_technology = require("__khaoslib__.technology")
+
+-- Lock burner biochamber behind Gleba research
+khaoslib_technology:load("biochamber")
+  :remove_unlock_recipe(function(effect)
+    return effect.type == "unlock-recipe" and effect.recipe:match("burner%-biochamber")
+  end, {all = true})
+  :commit()
+
+khaoslib_technology:load({
+  type = "technology",
+  name = "burner-biochamber",
+  icons = {
+    {icon = "__space-age__/graphics/technology/biochamber.png", icon_size = 256, tint = {r = 0.3, g = 0.3, b = 0.3}},
+  },
+  prerequisites = {"agricultural-science-pack"},
+  unit = {
+    time = 60,
+    count = 1000,
+    ingredients = {
+      {"wood-science-pack", 1},
+      {"steam-science-pack", 1},
+      {"automation-science-pack", 1},
+      {"logistic-science-pack", 1},
+      {"chemical-science-pack", 1},
+      {"space-science-pack", 1},
+      {"agricultural-science-pack", 1},
+    },
+  },
+}):add_unlock_recipe("burner-biochamber")
+  :add_unlock_recipe("burner-biochamber-to-biochamber")
+  :commit()
