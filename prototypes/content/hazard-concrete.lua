7d6ccdd5ff34d155715cd6837c61aa5182638d2b
Em Karisch <emii.khaos@gmail.com>
2026-07-04 09:17:41 +0200

    feat: add muluna

---
+++ prototypes/content/hazard-concrete.lua
@@ -0,0 +1,12 @@
+-- Reduce speed for hazard concrete variants similar to py
+data.raw["tile"]["hazard-concrete-left"].walking_speed_modifier = 0.5
+data.raw["tile"]["hazard-concrete-left"].vehicle_friction_modifier = 2
+
+data.raw["tile"]["hazard-concrete-right"].walking_speed_modifier = 0.5
+data.raw["tile"]["hazard-concrete-right"].vehicle_friction_modifier = 2
+
+data.raw["tile"]["refined-hazard-concrete-left"].walking_speed_modifier = 0.5
+data.raw["tile"]["refined-hazard-concrete-left"].vehicle_friction_modifier = 500
+
+data.raw["tile"]["refined-hazard-concrete-right"].walking_speed_modifier = 0.5
+data.raw["tile"]["refined-hazard-concrete-right"].vehicle_friction_modifier = 500
---
+++ prototypes/content/mooncrete.lua
@@ -0,0 +1,3 @@
+-- Same walking speed as their concrete equivalents for mooncrete
+data.raw["tile"]["mooncrete"].walking_speed_modifier = data.raw["tile"]["concrete"].walking_speed_modifier
+data.raw["tile"]["refined-mooncrete"].walking_speed_modifier = data.raw["tile"]["refined-concrete"].walking_speed_modifier
