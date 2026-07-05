1c48637f0232462478e0cc3c72590ab319ed0c73
Em Karisch <emii.khaos@gmail.com>
2026-07-05 10:04:52 +0200

    fix: give mooncrete its own tech

--- prototypes/content/mooncrete.lua
+++ prototypes/content/mooncrete.lua
@@ -1,3 +1,40 @@
+khaoslib_recipe = require("__khaoslib__.recipe")
+khaoslib_technology = require("__khaoslib__.technology")
+
 -- Same walking speed as their concrete equivalents for mooncrete
 data.raw["tile"]["mooncrete"].walking_speed_modifier = data.raw["tile"]["concrete"].walking_speed_modifier
 data.raw["tile"]["refined-mooncrete"].walking_speed_modifier = data.raw["tile"]["refined-concrete"].walking_speed_modifier
+
+-- Create new technology for mooncrete and refined mooncrete
+khaoslib_recipe:load("mooncrete")
+  :remove_unlock("muluna-advanced-stone-processing")
+  :commit()
+
+khaoslib_recipe:load("refined-mooncrete")
+  :remove_unlock("muluna-alumina-processing")
+  :commit()
+
+khaoslib_technology:load {
+  type = "technology",
+  name = "mooncrete",
+  icon = "__khaosmassive__/graphics/mooncrete/mooncrete_tech.png",
+  icon_size = 256,
+  prerequisites = {
+    "muluna-regolith-digging",
+    "muluna-alumina-processing",
+    "wood-gas-processing",
+    "space-science-pack",
+  },
+  unit = {
+    time = 30,
+    count = 100,
+    ingredients = {
+      {"automation-science-pack", 1},
+      {"logistic-science-pack", 1},
+      {"chemical-science-pack", 1},
+      {"space-science-pack", 1},
+    },
+  },
+} :add_unlock_recipe("mooncrete")
+  :add_unlock_recipe("refined-mooncrete")
+  :commit()
