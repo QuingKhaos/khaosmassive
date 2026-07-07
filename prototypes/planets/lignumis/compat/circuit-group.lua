bee52aa7f59f0897f3c496f0207796ebca043e4d
Em Karisch <emii.khaos@gmail.com>
2026-07-07 15:40:10 +0200

    fix: correct subgroup for burner lamp

--- prototypes/planets/lignumis/compat/circuit-group.lua
+++ prototypes/planets/lignumis/compat/circuit-group.lua
@@ -1,6 +1,12 @@
-data.raw["item"]["burner-lamp"].subgroup = "circuit-visual"
-data.raw["recipe"]["burner-lamp"].subgroup = "circuit-visual"
-data.raw["recipe"]["burner-lamp-copper"].subgroup = "circuit-visual"
+if settings.startup["Schall-CGP-individual-combinator-subgroups"] then
+  data.raw["item"]["burner-lamp"].subgroup = "circuit-visual-lamp"
+  data.raw["recipe"]["burner-lamp"].subgroup = "circuit-visual-lamp"
+  data.raw["recipe"]["burner-lamp-copper"].subgroup = "circuit-visual-lamp"
+else
+  data.raw["item"]["burner-lamp"].subgroup = "circuit-visual"
+  data.raw["recipe"]["burner-lamp"].subgroup = "circuit-visual"
+  data.raw["recipe"]["burner-lamp-copper"].subgroup = "circuit-visual"
+end
 
 data.raw["item"]["active-noise-cancelling-tower"].subgroup = "circuit-auditory"
 data.raw["recipe"]["active-noise-cancelling-tower"].subgroup = "circuit-auditory"
