6006ab5e5541dd4a0a9ab37ad7cfb70acde34e86
Em Karisch <emii.khaos@gmail.com>
2026-07-08 10:36:04 +0200

    feat: ensure usage of optional mods for compatible mods

---
+++ data.lua
@@ -0,0 +1,14 @@
+-- Check if optional khaosmassive mods are missing, but the compatible mod is present
+local mod_list = {
+    ["khaosmassive-factorissimo"] = "factorissimo-2-notnotmelon",
+    ["khaosmassive-frozen-goodies"] = "frozen-goodies",
+    ["khaosmassive-lane-balancers"] = "lane-balancers",
+    ["khaosmassive-loaders-modernized"] = "loaders-modernized",
+    ["khaosmassive-quantum-fabricator"] = "quantum-fabricator",
+}
+
+for mod_name, compatible_mod in pairs(mod_list) do
+    if not mods[mod_name] and mods[compatible_mod] then
+        log(mod_name .. " is missing, but " .. compatible_mod .. " is present. Please install " .. mod_name .. ".")
+    end
+end
